f9ca6ee6319fb16f0b67cab298bc0708ef9c7f5b
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2026-08-12 03:29:55 -0300

    Update app.js

--- project-will/web-client/js/app.js
+++ project-will/web-client/js/app.js
@@ -164,7 +164,7 @@ export async function InitApp() {
     //    }
     //}
 
-    engine.scene.add(world);
+    //engine.scene.add(world);
 
     world.addEntityToTile(0, 0, new Entity(square_geometry, tile_sheet_material, null, grass_tile_sprite1));
     world.addEntityToTile(0, 0, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
@@ -175,6 +175,11 @@ export async function InitApp() {
     world.addEntityToTile(0, -1, new Entity(square_geometry, tile_sheet_material, null, shallow_water_tile_sprite1));
     world.addEntityToTile(0, -2, new Entity(square_geometry, misc_sheet_material, null, fire_sprite1));
     world.addEntityToTile(0, 2, new Entity(square_geometry, misc_sheet_material, null, fire_sprite2));
+
+    const entt = new Entity();
+    entt.children.push(new Entity(square_geometry, tile_sheet_material, new Mat4(), grass_tile_sprite1));
+    entt.children.push(new Entity(square_geometry, creatures1_sheet_material, new Mat4(), cat_sprite1));
+    engine.scene.add(entt);
 
     // Create Sprite for Fire
     // const fireSprite = new Sprite(sheet);
