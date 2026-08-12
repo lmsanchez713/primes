1549c5be26757e2e4b85488db2303b8ec7c876a0
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2026-08-12 04:40:15 -0300

    textures bug

--- project-will/web-client/js/app.js
+++ project-will/web-client/js/app.js
@@ -168,13 +168,20 @@ export async function InitApp() {
 
     world.addEntityToTile(0, 0, new Entity(square_geometry, tile_sheet_material, null, grass_tile_sprite1));
     world.addEntityToTile(0, 0, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
-    //engine.scene.add(new Entity(square_geometry, creatures1_sheet_material, new Mat4(), cat_sprite1));
     world.addEntityToTile(-1, 0, new Entity(square_geometry, tile_sheet_material, null, dirt_tile_sprite1));
     world.addEntityToTile(0, 1, new Entity(square_geometry, tile_sheet_material, null, stone_tile_sprite1));
     world.addEntityToTile(1, 0, new Entity(square_geometry, tile_sheet_material, null, water_tile_sprite1));
     world.addEntityToTile(0, -1, new Entity(square_geometry, tile_sheet_material, null, shallow_water_tile_sprite1));
     world.addEntityToTile(-1, 0, new Entity(square_geometry, misc_sheet_material, null, fire_sprite1));
     world.addEntityToTile(0, 1, new Entity(square_geometry, misc_sheet_material, null, fire_sprite2));
+
+    world.addEntityToTile(-1, 0, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
+    world.addEntityToTile(0, 1, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
+    world.addEntityToTile(1, 0, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
+    world.addEntityToTile(0, -1, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
+    world.addEntityToTile(1, 1, new Entity(square_geometry, creatures1_sheet_material, null, cat_sprite1));
+
+    //engine.scene.add(new Entity(square_geometry, creatures1_sheet_material, new Mat4(), cat_sprite1));
 
     //const entt = new Entity();
     //entt.children.push(new Entity(square_geometry, tile_sheet_material, new Mat4(), grass_tile_sprite1));
