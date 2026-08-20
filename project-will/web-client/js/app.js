64b15a335ed072d05b2e041d859b7cefaac37517
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2026-08-20 13:55:56 -0300

    primitive.js created

--- project-will/web-client/js/app.js
+++ project-will/web-client/js/app.js
@@ -10,86 +10,11 @@ import { MapLoader } from './scene/map_loader.js';
 import { ItemType } from './scene/item.js';
 import { createSquareGeometry } from './core/geometry.js';
 import { UBOManager } from './core/ubo.js';
+import { Primitive_Engine, Primitive } from './primitive.js';
 
 let engine;
 let uboManager = null;
 let sceneUBO = null;
-
-function draw_primitive(primitive) {
-    //console.log(primitive);
-    if (!primitive.enabled) return;
-}
-
-class Primitive {
-    constructor(engine, parameters = {}) {
-        this.engine = engine;
-        this.name = parameters.name ?? "";
-        this.shader_program = parameters.shader_program ?? null;
-        this.vao = parameters.vao ?? null;
-        this.uniforms = parameters.uniforms ?? null;
-        this.uniform_blocks = parameters.uniform_blocks ?? null;
-        this.engine_flags = parameters.engine_flags ?? null;
-        this.draw_intervals = parameters.draw_intervals ?? null;
-        this.draw_algorithm = parameters.draw_algorithm ?? draw_primitive;
-        this.enabled = parameters.enabled ?? true;
-        this.children = parameters.children ?? [];
-    }
-    render() {
-        this.draw_algorithm(this);
-    }
-}
-
-class Primitive_Engine {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.gl = canvas.getContext('webgl2');
-        if (!this.gl) {
-            console.error('WebGL2 not supported');
-            return;
-        }
-        this.is_running = false;
-        this.primitives = [];
-        this.shaders = {};
-        this.geometries = {};
-    }
-
-    start() {
-        this.is_running = true;
-        this._loop = this._loop.bind(this);
-        requestAnimationFrame(this._loop);
-    }
-
-    stop() {
-        this.is_running = false;
-    }
-
-    _loop(timestamp) {
-        //console.log("Loop timestamp:", timestamp);
-        if (!this.is_running) return;
-
-        const delta_time = this.last_timestamp ? (timestamp - this.last_timestamp) / 1000 : 0;
-        this.last_timestamp = timestamp;
-
-        this.render();
-
-        requestAnimationFrame(this._loop);
-    }
-
-    render() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-
-        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);// | gl.DEPTH_BUFFER_BIT);
-
-        // Update projection based on mode and canvas aspect ratio
-        const aspect = this.canvas.width / this.canvas.height;
-        for (const primitive of this.primitives) {
-            primitive.render();
-        }
-    }
-}
 
 export async function InitApp() {
     const canvas = document.getElementById('glCanvas');
