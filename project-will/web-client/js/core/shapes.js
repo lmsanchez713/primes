8b2f58aa2948f8958e0d35adcdbad674fbd7c981
lmsanchez713 <43467466+lmsanchez713@users.noreply.github.com>
2026-08-29 14:01:29 -0300

    Update shapes.js

--- project-will/web-client/js/core/shapes.js
+++ project-will/web-client/js/core/shapes.js
@@ -1,4 +1,4 @@
-import { Geometry } from '../ogl2.js';
+import { Geometry } from '/js/core/geometry.js';
 
 export function generateCuboidVertices(p0, p1, p2, p3, p4, p5, p6, p7) {
     const vertices = new Float32Array();
@@ -9,7 +9,29 @@ export function generateCuboidVertices(p0, p1, p2, p3, p4, p5, p6, p7) {
     return vertices;
 }
 
-export function createCubeGeometry(engine) {
+export function generate_triangle(p0, p1, p2) {
+    //
+}
+
+export function generateSea(side_length, senoid) {
+    const total_vertices = side_length * side_length * 6;
+    const positions = new Float32Array(total_vertices * 3),
+        normals = new Float32Array(total_vertices * 3),
+        textures = new Float32Array(total_vertices * 2);
+
+    const pos_start = -side_length / 2.0;
+    for (let y = 0; y < side_length; y++) {
+        for (let x = 0; x < side_length; x++) {
+            let px0 = pos_start + x, pz0 = pos_start + y, px1 = pos_start + x + 1.0, pz1 = pos_start + y + 1.0;
+            let v3_offset = (y * side_length + x) * 6 * 3, v2_offset = (y * side_length + x) * 6 * 2;
+            positions[v3_offset];
+        }
+    }
+
+    return { positions, normals, textures };
+}
+
+export function createCubeGeometry(engine, keep_on_ram = false) {
     // Vertices for two triangles forming a quad
     const vertices = new Float32Array([
         -0.5, -0.5, 0.5,
@@ -83,7 +105,7 @@ export function createCubeGeometry(engine) {
         0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1
     ]);
 
-    const geo = new Geometry(engine, engine.gl.TRIANGLES);
+    const geo = new Geometry(engine, keep_on_ram);
     geo.addBuffer('aPosition', vertices, 3);
     geo.addBuffer('aTexCoord', texCoords, 2);
     geo.addBuffer('aNormal', normals, 3);
